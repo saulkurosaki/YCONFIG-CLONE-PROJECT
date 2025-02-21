4eeb555721948ca0a6b1eeed3e6aec0d0a49500c
saulkurosaki <saulkurosaki13@gmail.com>
2025-02-21 16:26:22 -0600

    implement the root route group layout.tsx initial component

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return <main className="font-work-sans">{children}</main>;
+}
