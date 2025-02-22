9303643452eb709a4f94468589285c2efa6f1e73
saulkurosaki <saulkurosaki13@gmail.com>
2025-02-22 13:58:40 -0600

    feat(SearchForm): implement some of the lucid-react library images for the buttons

--- app/components/SearchFormReset.tsx
+++ app/components/SearchFormReset.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { X } from "lucide-react";
 import Link from "next/link";
 
 const SearchFormReset = () => {
@@ -11,7 +12,7 @@ const SearchFormReset = () => {
   return (
     <button type="reset" onClick={reset}>
       <Link href="/" className="search-btn text-white">
-        X
+        <X className="size-5" />
       </Link>
     </button>
   );
