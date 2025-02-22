de1c3b45673d93afe8daca5c36e6ff77dc653f43
saulkurosaki <saulkurosaki13@gmail.com>
2025-02-22 14:22:16 -0600

    feat(Home): implement the startup cards section posts maping structure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,6 +6,21 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const posts = [
+    {
+      _createdAt: "Yesterday",
+      views: 55,
+      author: {
+        _id: 1,
+      },
+      _id: 1,
+      description: "This is a description",
+      image:
+        "https://images.unsplash.com/photo-1595433707802-6b2626ef1c91?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
 
   return (
     <>
@@ -25,6 +40,14 @@ export default async function Home({
         <p className="text-30-semibold">
           {query ? `Search results for "${query}"` : "All Startups"}
         </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => <StartupCard />)
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
