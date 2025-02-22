19da241306e85396bb5143f482eeea0f100e8489
saulkurosaki <saulkurosaki13@gmail.com>
2025-02-21 20:17:18 -0600

    feat(SearchFormReset.tsx): implement the complete SearchFormReset component

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -1,12 +1,8 @@
 import Form from "next/form";
+import SearchFormReset from "./SearchFormReset";
 
 const SearchForm = () => {
   const query = "Test";
-
-  const reset = () => {
-    const form = document.querySelector(".search-form") as HTMLFormElement;
-    if (form) form.reset();
-  };
 
   return (
     <Form action="/" scroll={false} className="search-form">
@@ -17,9 +13,7 @@ const SearchForm = () => {
         placeholder="Search Startups"
       />
 
-      <div className="flex gap-2">
-        {query && <button type="reset" onClick={reset}></button>}
-      </div>
+      <div className="flex gap-2">{query && <SearchFormReset />}</div>
     </Form>
   );
 };
--- app/components/SearchFormReset.tsx
+++ app/components/SearchFormReset.tsx
@@ -1,5 +1,20 @@
+"use client";
+
+import Link from "next/link";
+
 const SearchFormReset = () => {
-  return <div>SearchFormReset</div>;
+  const reset = () => {
+    const form = document.querySelector(".search-form") as HTMLFormElement;
+    if (form) form.reset();
+  };
+
+  return (
+    <button type="reset" onClick={reset}>
+      <Link href="/" className="search-btn text-white">
+        X
+      </Link>
+    </button>
+  );
 };
 
 export default SearchFormReset;
