5a9bfdd758d5b8cf01d82d6a9e799243962a9595
saulkurosaki <saulkurosaki13@gmail.com>
2025-03-02 16:47:19 -0600

    feat(StartupDetaisPage): implement the StartupDetaisPage initial structure

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,5 +1,14 @@
-const StartupDetailsPage = () => {
-  return <div>StartupDetailsPage</div>;
+const StartupDetailsPage = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  const id = (await params).id;
+  return (
+    <>
+      <h1 className="text-3xl">This is a Startup number = {id}</h1>
+    </>
+  );
 };
 
 export default StartupDetailsPage;
