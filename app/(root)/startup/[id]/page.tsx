9e2e1f0393d00d66bc2b8b55c29fdee15430f8a2
saulkurosaki <saulkurosaki13@gmail.com>
2025-03-02 17:02:53 -0600

    feat(StartupDetailsPage): implement the experimental Partial Pre-Rendering strategy along displaying the sanity server fetched post title

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,12 +1,22 @@
+import { client } from "@/sanity/lib/client";
+import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
+import { notFound } from "next/navigation";
+
+export const experimental_ppr = true;
+
 const StartupDetailsPage = async ({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) => {
   const id = (await params).id;
+  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+  if (!post) return notFound();
+
   return (
     <>
-      <h1 className="text-3xl">This is a Startup number = {id}</h1>
+      <h1 className="text-3xl">{post.title}</h1>
     </>
   );
 };
