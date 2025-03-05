81aaec26de6b1a9f940cbda1cfb1d856807f7a72
saulkurosaki <saulkurosaki13@gmail.com>
2025-03-05 14:45:44 -0600

    feat(View): implement the initial structure of the component

--- components/View.tsx
+++ components/View.tsx
@@ -1,5 +1,9 @@
-const View = () => {
-  return <div>View</div>;
+const View = ({ id }: { id: string }) => {
+  return (
+    <div className="view-container">
+      <div className="absolute  -top-2 -right-2"></div>
+    </div>
+  );
 };
 
 export default View;
