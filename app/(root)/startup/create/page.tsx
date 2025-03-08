c773695c0320e97a353c94c288d8d36f72dfe75d
saulkurosaki <saulkurosaki13@gmail.com>
2025-03-07 18:28:11 -0600

    feat(CreateStartupPage): implement the create startup page pink title

--- app/(root)/startup/create/page.tsx
+++ app/(root)/startup/create/page.tsx
@@ -1,5 +1,11 @@
 const CreateStartupPage = () => {
-  return <div>CreateStartupPage</div>;
+  return (
+    <>
+      <section className="pink_container !min-h-[230px]">
+        <h1 className="heading">Submit Your Startup</h1>
+      </section>
+    </>
+  );
 };
 
 export default CreateStartupPage;
