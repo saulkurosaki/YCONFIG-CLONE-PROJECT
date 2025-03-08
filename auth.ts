5a252b6ca8c1a03c95614a6821d243da0991dee8
saulkurosaki <saulkurosaki13@gmail.com>
2025-03-07 18:22:48 -0600

    fix(auth.js): fix the auth nextjs caching issue

--- auth.ts
+++ auth.ts
@@ -11,9 +11,11 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       user: { name, email, image },
       profile: { id, login, bio },
     }) {
-      const existingUser = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
-        id,
-      });
+      const existingUser = await client
+        .withConfig({ useCdn: false })
+        .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+          id,
+        });
 
       if (!existingUser) {
         await writeClient.create({
@@ -31,9 +33,11 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     },
     async jwt({ token, account, profile }) {
       if (account && profile) {
-        const user = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
-          id: profile?.id,
-        });
+        const user = await client
+          .withConfig({ useCdn: false })
+          .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+            id: profile?.id,
+          });
 
         token.id = user?._id;
       }
