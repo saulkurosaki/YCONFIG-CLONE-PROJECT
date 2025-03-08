fb53956093a890db2b24e72b8b8ec9582670bc12
saulkurosaki <saulkurosaki13@gmail.com>
2025-03-08 00:03:53 -0600

    feat(StartupForm): implement the description label and shadcn textarea field along the error handler for the description

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
 
 const StartupForm = () => {
   const [errors, setErrors] = useState<Record<string, string>>({});
@@ -22,6 +23,53 @@ const StartupForm = () => {
 
         {errors.title && <p className="startup-form_error">{errors.title}</p>}
       </div>
+
+      <div>
+        <label htmlFor="description" className="startup-form_label">
+          Description
+        </label>
+        <Textarea
+          id="description"
+          name="description"
+          className="startup-form_textarea"
+          required
+          placeholder="Startup Description"
+        />
+
+        {errors.description && (
+          <p className="startup-form_error">{errors.description}</p>
+        )}
+      </div>
+
+      {/* <div>
+        <label htmlFor="title" className="startup-form_label">
+          Title
+        </label>
+        <Input
+          id="title"
+          name="title"
+          className="startup-form_input"
+          required
+          placeholder="Startup Title"
+        />
+
+        {errors.title && <p className="startup-form_error">{errors.title}</p>}
+      </div> */}
+
+      {/* <div>
+        <label htmlFor="title" className="startup-form_label">
+          Title
+        </label>
+        <Input
+          id="title"
+          name="title"
+          className="startup-form_input"
+          required
+          placeholder="Startup Title"
+        />
+
+        {errors.title && <p className="startup-form_error">{errors.title}</p>}
+      </div> */}
     </form>
   );
 };
