11d3bd0610d15fe7028c2a5f3d8056175b3636b9
saulkurosaki <saulkurosaki13@gmail.com>
2025-03-08 00:19:47 -0600

    feat(StartupForm): implement the pitch label and uiw mdeditor field along the error handler for the pitch

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -3,9 +3,11 @@
 import { useState } from "react";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
+import MDEditor from "@uiw/react-md-editor";
 
 const StartupForm = () => {
   const [errors, setErrors] = useState<Record<string, string>>({});
+  const [pitch, setPitch] = useState("**Hello World!!!**");
 
   return (
     <form action={() => {}} className="startup-form">
@@ -71,6 +73,28 @@ const StartupForm = () => {
         />
 
         {errors.link && <p className="startup-form_error">{errors.link}</p>}
+      </div>
+
+      <div data-color-mode="light">
+        <label htmlFor="pitch" className="startup-form_label">
+          Pitch
+        </label>
+        <MDEditor
+          value={pitch}
+          onChange={(value) => setPitch(value as string)}
+          id="pitch"
+          preview="edit"
+          height={300}
+          style={{ borderRadius: 20, overflow: "hidden" }}
+          textareaProps={{
+            placeholder: "Briefly describe your idea and what problem it solves"
+          }}
+          previewOptions={{
+            disallowedElements: ["style"],
+          }}
+        />
+
+        {errors.pitch && <p className="startup-form_error">{errors.pitch}</p>}
       </div>
     </form>
   );
