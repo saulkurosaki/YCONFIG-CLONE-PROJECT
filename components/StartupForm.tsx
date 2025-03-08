0810f33713022cb9ece70f251526a7b484ccf3f3
saulkurosaki <saulkurosaki13@gmail.com>
2025-03-08 00:00:37 -0600

    feat(StartupForm): implement the title label and shadcn input field along the error handler for the title

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -1,5 +1,29 @@
+"use client";
+
+import { useState } from "react";
+import { Input } from "./ui/input";
+
 const StartupForm = () => {
-  return <div>StartupForm</div>;
+  const [errors, setErrors] = useState<Record<string, string>>({});
+
+  return (
+    <form action={() => {}} className="startup-form">
+      <div>
+        <label htmlFor="title" className="startup-form_label">
+          Title
+        </label>
+        <Input
+          id="title"
+          name="title"
+          className="startup-form_input"
+          required
+          placeholder="Startup Title"
+        />
+
+        {errors.title && <p className="startup-form_error">{errors.title}</p>}
+      </div>
+    </form>
+  );
 };
 
 export default StartupForm;
