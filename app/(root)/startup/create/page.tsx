60c37e5b13e69c2149a6cb586b6c67f90a9c2fb8
saulkurosaki <saulkurosaki13@gmail.com>
2025-03-07 23:27:37 -0600

    feat(CreateStartupPage): implement the session validation or redirection to home page

--- app/(root)/startup/create/page.tsx
+++ app/(root)/startup/create/page.tsx
@@ -1,9 +1,19 @@
-const CreateStartupPage = () => {
+import { auth } from "@/auth";
+import StartupForm from "@/components/StartupForm";
+import { redirect } from "next/navigation";
+
+const CreateStartupPage = async () => {
+  const session = await auth();
+
+  if (!session) redirect("/");
+
   return (
     <>
       <section className="pink_container !min-h-[230px]">
         <h1 className="heading">Submit Your Startup</h1>
       </section>
+
+      <StartupForm />
     </>
   );
 };
