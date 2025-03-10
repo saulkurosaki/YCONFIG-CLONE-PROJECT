4259350f826db873b5de5f03782b0175805da938
saulkurosaki <saulkurosaki13@gmail.com>
2025-03-10 12:12:58 -0600

    feat(actions.ts): implement the server actions file along the createPitch server action function initial structure

---
+++ lib/actions.ts
@@ -0,0 +1,10 @@
+"use server"
+
+import { auth } from "@/auth";
+import { parseServerActionResponse } from "./utils";
+
+export const createPitch = async (state: any, form: FormData, pitch: string) => {
+  const session = await auth();
+
+  if (!session) return parseServerActionResponse({ error: "Not signed in", status: "ERROR" });
+};
