71659c545d8d443200eb6e4ea543132be91941a5
saulkurosaki <saulkurosaki13@gmail.com>
2025-03-12 13:06:58 -0600

    feat(ProfileDetailsPage): implement the profile user startups seccion initial structure

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -4,6 +4,8 @@ import { AUTHOR_BY_ID_QUERY } from "@/sanity/lib/queries";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
+export const experimental_ppr = true;
+
 const ProfileDetailsPage = async ({
   params,
 }: {
@@ -36,6 +38,15 @@ const ProfileDetailsPage = async ({
             @{user?.username}
           </p>
           <p className="mt-1 text-center text-14-normal">{user?.bio}</p>
+        </div>
+
+        <div className="flex-1 flex flex-col gap-5 lg:-mt-5">
+          <p className="text-30-bold">
+            {session?.id === id ? "Your" : "All"} Startups
+          </p>
+          <ul className="card_grid-sm">
+            {/* TODO: ADD USER STARTUPS */}
+          </ul>
         </div>
       </section>
     </>
