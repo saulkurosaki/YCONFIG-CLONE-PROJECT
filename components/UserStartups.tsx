11b88221d0e3d1d261d4632236589ccba18a8d6f
saulkurosaki <saulkurosaki13@gmail.com>
2025-03-12 13:21:24 -0600

    feat(UserStartups): implement the user startups map rendering

--- components/UserStartups.tsx
+++ components/UserStartups.tsx
@@ -1,7 +1,21 @@
-const UserStartups = () => {
-  return (
-    <div>UserStartups</div>
-  )
-}
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_BY_AUTHOR_QUERY } from "@/sanity/lib/queries";
+import StartupCard, { StartupTypeCard } from "./StartupCard";
 
-export default UserStartups
+const UserStartups = async ({ id }: { id: string }) => {
+  const startups = await client.fetch(STARTUPS_BY_AUTHOR_QUERY, { id });
+
+  return (
+    <>
+      {startups.length > 0 ? (
+        startups.map((startup: StartupTypeCard) => (
+          <StartupCard key={startup._id} post={startup} />
+        ))
+      ) : (
+        <p className="no-result">No posts yet</p>
+      )}
+    </>
+  );
+};
+
+export default UserStartups;
