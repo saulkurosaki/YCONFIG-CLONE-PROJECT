b340ec5b5a38ed9dfb75b605f3e154a35f6d606e
saulkurosaki <saulkurosaki13@gmail.com>
2025-03-12 12:58:54 -0600

    feat(ProfileDetailsPage): implement the profile user name display

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -1,5 +1,31 @@
-const ProfileDetailsPage = () => {
-  return <div>ProfileDetailsPage</div>;
+import { auth } from "@/auth";
+import { client } from "@/sanity/lib/client";
+import { AUTHOR_BY_ID_QUERY } from "@/sanity/lib/queries";
+import { notFound } from "next/navigation";
+
+const ProfileDetailsPage = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  const id = (await params).id;
+  const session = await auth();
+
+  const user = await client.fetch(AUTHOR_BY_ID_QUERY, { id });
+  if (!user) return notFound();
+  return (
+    <>
+      <section className="profile_container">
+        <div className="profile_card">
+          <div className="profile_title">
+            <h3 className="text-24-black uppercase text-center line-clamp-1">
+              {user.name}
+            </h3>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default ProfileDetailsPage;
