28bb47fdb3099d9c5696408e34d82af695ef1b1d
saulkurosaki <saulkurosaki13@gmail.com>
2025-03-17 21:52:44 -0600

    vercel deployment errors fixes 3

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["lucide-react"],
+  transpilePackages: ["lucide-react", "sanity"],
   images: {
     domains: ["cdn.sanity.io", "avatars.githubusercontent.com"],
+  },
+  experimental: {
+    esmExternals: "loose",
   },
 };
 
--- sanity.config.ts
+++ sanity.config.ts
@@ -1,34 +1,26 @@
-"use client";
-
 /**
  * This configuration is used to for the Sanity Studio that's mounted on the `/app/studio/[[...tool]]/page.tsx` route
  */
 
 import { defineConfig } from "sanity";
-import { deskTool } from "sanity/desk";
+import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
 import { schema } from "./sanity/schemaTypes";
 import { markdownSchema } from "sanity-plugin-markdown";
-
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from "./sanity/env";
 import { structure } from "./sanity/structure";
-import { structureTool } from "sanity/structure";
 
-export default defineConfig({
+const config = defineConfig({
   name: "default",
   title: "your-project-title",
-
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-
+  projectId,
+  dataset,
   plugins: [
     structureTool({ structure }),
-    // Vision is for querying with GROQ from inside the Studio
-    // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
     markdownSchema(),
   ],
-
   schema,
 });
+
+export default config;
